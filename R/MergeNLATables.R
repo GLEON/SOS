2caa4d8365415c2ab96c155a1c40a53bfb9d9d66
hdugan <hilarydugan@gmail.com>
2015-07-09 10:09:19 -0500

    Create function to calculate log-log regression for each state in NLA dataset. Output slope, intercept, R2 in datatable. Create plot of log(DOC)~log(SECMEAN) for each state; one state per panel.

--- R/MergeNLATables.R
+++ R/MergeNLATables.R
@@ -4,9 +4,12 @@
 # Author: Ian McCullough, immccull@gmail.com
 
 # Ian's working directory
-setwd('H:/Ian_GIS/gleon/SOS/Data')
+#setwd('H:/Ian_GIS/gleon/SOS/Data')
 
-# Read in water quality data file
+# Kait's working directory
+setwd('/Users/FarrellKJ/Desktop/R/SOS/Data')
+
+# Read in water quality data file ####
 wq = read.csv('NTL water chem.csv', header=T)
 
 # Read in sampled lake information file
@@ -26,7 +29,36 @@ merger = merger[, -grep("STATE_NAME", colnames(merger[2:length(merger)]))] # del
 # Output to csv
 write.csv(merger, 'NTL_water_chem_allstates.csv')
 
-## Subset by state name and output state-specific CSV##
+# Split data by state into new list 'by_state' ####
+library(plyr)
+splt.by <- c('STATE_NAME')
+by_state <- split(merger, merger[,splt.by] )
+
+# Run log-log model for each state, output as dataframe 'state_slopes' ####
+# function will output slope, int., and R2 value for log(DOC)~log(Secchi) for each state
+# p-value indicates whether slope is different from zero
+state_slopes <- ldply(by_state, function(by_state) {
+  lm1 <-lm((log(DOC))~log(SECMEAN), data=by_state)
+  N    = (df.residual(lm1)+2) # Number of data points per state
+  r.sq <- summary(lm1)$r.squared # R-squared value for model fit
+  slope <- (summary(lm1)$coefficients[2]) # Slope of log-log model
+  int <- (summary(lm1)$coefficients[1]) # Intercept of log-log model
+  Fv <- as.numeric((summary(lm1))$fstatistic) 
+  ss <- sum(is.nan(Fv)) >0 
+  P <- ifelse(!ss, 1-pf(Fv[1],Fv[2],Fv[3]),NA) #Is slope sig. diff. from zero
+  data.frame(N, slope, int, r.sq, P)
+})
+colnames(state_slopes) <- c('State', 'N', 'Slope', 'Intercept', 'R-Squared', 'P-Value')
+
+write.csv(state_slopes, 'NTL_water_chem_model_slopes_allstates.csv')
+
+# Plot each state on its own panel ####
+library(lattice)
+xyplot(log(DOC)~log(SECMEAN)|STATE_NAME, group=STATE_NAME, data=merger,
+       type=c('r','p'), pch=15, lwd=2, layout=c(4,3,4),
+       scales=list(alternating=1,tck=c(1,0), cex=1))
+
+## Subset by state name and output state-specific CSV####
 Ohio = subset(merger, grepl("Ohio", merger$STATE_NAME), select=STATE_NAME:FID_1)
 write.csv(Ohio, "OhioNLA.csv")
 
