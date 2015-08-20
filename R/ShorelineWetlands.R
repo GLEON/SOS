ae3563d764ffdb24feae174bb949c6a2871418d9
immccull <immccull@gmail.com>
2015-08-20 14:17:59 -0700

    stupid, darn commas

--- R/ShorelineWetlands.R
+++ R/ShorelineWetlands.R
@@ -70,7 +70,7 @@ for(i in 1:nrow(lakebuffer)) {
 
 wetland_length = cell_size*x #multiply num of cells by cell length specified above
 #forest_length = cell_size*y
-shoreline = data.frame(lake=lakes$WATERBODY_) #
+shoreline = data.frame(lake=lakes$WATERBODY_)
 shoreline$area_sqm = lakes$SHAPE_Area
 shoreline$perim_m=lakes$SHAPE_Leng
 shoreline$wetland_m=wetland_length
