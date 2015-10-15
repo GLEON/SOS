c98bb2d443f9266988c32d89eb6800df7045a6da
derekrob <dcroberts@ucdavis.edu>
2015-10-14 19:51:16 -0700

    Update to respiration routine. Change of name for GPP to NPP to reflect calculations. Added parameteter for updated Resp routine. General housekeeping.

--- R/Model/SOS_Resp.R
+++ R/Model/SOS_Resp.R
@@ -1,19 +1,22 @@
 
-Resp <- function(DOC,CHL){
+Resp <- function(DOC,temp,RespParam){
   #DOC in g/m3
   #CHL in mg/m3 or ug/L
   
-  logR <- -0.92 + 0.41*log(CHL) + 0.30*log(DOC)
+  #logR <- -0.92 + 0.41*log(CHL) + 0.30*log(DOC)
   
   #logR <- -0.81 + 0.56*log(CHL)
+  TempCorr <- 1.08^(T-20) #Temperature correction factor for DOC-respiration relationship
   
-  R <- 10^logR #mmol/m3/hr
+  R <- DOC*RespParam*TempCorr #g/m3
+  
+  #R <- 10^logR #mmol/m3/hr
 
-  R <- R*(1/1000)*(16)*(24/1) #g O2/m3/d 
+  #R <- R*(1/1000)*(16)*(24/1) #g O2/m3/d 
   #Output: R  (O2 mmol/m3/hr) --> * (1mol /1000 mmol) * (16g O2/mol) * (24h/1day)  -->  g/m3/d 
 
   #Assume O2 respired is one-to-one with grams of DOC removed
-  Resp_DOC_out <- R #g C/m3/d
+  #Resp_DOC_out <- R #g C/m3/d
 
-  return(Resp_DOC_out/10)  #divided by ten to avoid crashing program
+  return(R)  
 }
