515cf70764a31c53cd87479a3c29bf2bb76bcffe
hdugan <hilarydugan@gmail.com>
2016-03-28 16:18:07 -0500

    error in temp vs. true

--- R/Model/SOS_NPP.R
+++ R/Model/SOS_NPP.R
@@ -45,7 +45,7 @@ NPP<-function(CHL,P,PhoticDepth,WT)
     
   }
   
-  CHL <- CHL*PhoticDepth
+  #CHL <- CHL*PhoticDepth
   P <- P*PhoticDepth
   
   NPP_rate <- 10^(b0+b1*log10(CHL)+b2*WT) #mg C/m2/d
