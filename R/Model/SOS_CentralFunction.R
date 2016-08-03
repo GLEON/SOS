1efc9cb1997274b2a1e14b4eeccb8ec5b48b5414
Derek <Derek>
2016-08-02 17:42:55 -0700

    comments

--- R/Model/SOS_CentralFunction.R
+++ R/Model/SOS_CentralFunction.R
@@ -107,8 +107,8 @@ for (i in 1:(steps)){
   PhoticDepth <- log(100)/(1.7/InputData$Secchi[i]) #Calc photic depth as function of Secchi depth
   if (PhoticDepth>LakeDepth){PhoticDepth<-LakeDepth} #QC - If photic depth calc'ed as greater than lake depth, photic depth = lake depth
   RawProduction <- NPP(InputData$Chla[i],InputData$TP[i],PhoticDepth,InputData$EpiTemp[i],yday(InputData$datetime[i]),ProdStartDay,ProdEndDay) #mg C/m^2/d
-  NPPdata$DOC_rate[i] = RawProduction$NPP_DOC_rate
-  NPPdata$POC_rate[i] = RawProduction$NPP_POC_rate
+  NPPdata$DOC_rate[i] = RawProduction$NPP_DOC_rate #ACtually GPP!
+  NPPdata$POC_rate[i] = RawProduction$NPP_POC_rate #ACtually GPP!
   
   #Call SWGW Function
   SWGW <- SWGWFunction(Q_sw,Q_gw,Rainfall,AerialLoad, PropCanopy, LakePerimeter, WetlandLoad, PropWetlands, DOC_gw, 
