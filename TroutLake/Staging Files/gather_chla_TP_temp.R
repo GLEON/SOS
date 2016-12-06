f12a289225ef45a86914ba4d34b39e9056a3beb4
HILARY A DUGAN <hilarydugan@gmail.com>
2016-12-06 10:28:27 -0600

    updated trout data

--- TroutLake/Staging Files/gather_chla_TP_temp.R
+++ TroutLake/Staging Files/gather_chla_TP_temp.R
@@ -4,34 +4,37 @@
 library(xts)
 
 # Read in datafiles ####
-temp <- read.csv('./TroutLake/Staging Files/TL_Variables/TL_daily_water_temp.csv')
+epi <- read.csv('./TroutLake/Staging Files/TL_Variables/TL_daily_water_temp_epi.csv',stringsAsFactors = F)
+epi = epi[!is.na(epi$wtemp),]
+hypo <- read.csv('./TroutLake/Staging Files/TL_Variables/TL_daily_water_temp_hypo.csv',stringsAsFactors = F)
+hypo = hypo[!is.na(hypo$wtemp),]
 tp <- read.csv('./TroutLake/Staging Files/TL_Variables/TL_waterchem.csv')
 chla <- read.csv('./TroutLake/Staging Files/TL_Variables/TL_chla.csv')
 
 # Chlorophyll-a in ug/L (1 mg/m3=1ug/L) ####
 # Calculate mean water column chla for each available sampling date
 chla <- subset(chla, chla$chlor>=0) #Remove NAs and values <0
-chla.xts <- xts(x = chla$chlor,as.POSIXct(chla$sampledate))
+chla.xts <- xts(x = chla$chlor,as.Date(chla$sampledate))
 ep <- endpoints(chla.xts,'days')
 daily_chla <- period.apply(chla.xts,ep,mean) # ug/L
 
 # Total Phosphorus ug/L ####
 # Calculate mean water column TP for each available sampling date
 tp <- subset(tp, totpuf>=0) #Remove NA values
-tp.xts <- xts(x = tp$totpuf, as.POSIXct(tp$sampledate))
+tp.xts <- xts(x = tp$totpuf, as.Date(tp$sampledate))
 ep <- endpoints(tp.xts,'days')
 daily_tp <- period.apply(tp.xts,ep,mean) # ug/L
 
-# Temperature in lake ####
-epi <- subset(temp, depth<=2)
-hypo <- subset(temp, depth>=15)
+# # Temperature in lake ####
+# epi <- subset(temp, depth<=2)
+# hypo <- subset(temp, depth>=15)
 
 # Mean Epi and Hypo temp per date ####
-epi.xts <- xts(x = epi$wtemp,as.POSIXct(epi$sampledate))
+epi.xts <- xts(x = epi$wtemp,as.Date(strptime(epi$datetime,'%Y-%m-%d')))
 ep <- endpoints(epi.xts,'days')
 daily_epi <- period.apply(epi.xts,ep,mean) # C
 
-hypo.xts <- xts(x = hypo$wtemp,as.POSIXct(hypo$sampledate))
+hypo.xts <- xts(x = hypo$wtemp,as.Date(hypo$datetime))
 ep <- endpoints(hypo.xts,'days')
 daily_hypo <- period.apply(hypo.xts,ep,mean) # C
 
@@ -40,15 +43,16 @@ daily_hypo <- period.apply(hypo.xts,ep,mean) # C
 mod_start <- '2004-01-01'
 mod_end <- '2015-12-27'
 dates <- (seq(as.Date(mod_start), as.Date(mod_end), by='days'))
+# dates_xts <- as.xts(x = rep(NA,length(dates)), as.Date(dates))
 dates_xts <- as.xts(dates)
 
 # Merge xts dates, chla, tp, flow and convert to dataframe
-daily_data <- merge.xts(dates_xts, daily_hypo, daily_epi, daily_tp,  
+daily_data <- merge.xts(dates_xts,daily_hypo, daily_epi, daily_tp,  
                         daily_chla, fill='')
 daily_data <- data.frame(date=index(daily_data), coredata(daily_data))
 
-daily_data$date <- as.Date(daily_data$date, format = "%m/%d/%y")
-format(daily_data$date,"%m/%d/%Y")
+# daily_data$date <- as.Date(daily_data$date, format = "%m/%d/%y")
+# format(daily_data$date,"%m/%d/%Y")
 
 daily_tp_all <- as.data.frame(na.approx(daily_data$daily_tp, na.rm=F))
 daily_chla_all <- as.data.frame(na.approx(daily_data$daily_chla, na.rm=F))
