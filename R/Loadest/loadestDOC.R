120740a20656ffcafc28173099c8d782f429d3a3
hdugan <hdugan@wisc.edu>
2018-03-01 12:06:06 -0600

    harp results

--- R/Loadest/loadestDOC.R
+++ R/Loadest/loadestDOC.R
@@ -11,60 +11,73 @@ inflow = read.csv('C:/Users/hdugan/Documents/Rpackages/SOS/HarpLake/StagingFiles
 head(inflow)
 inflow$SDATE = as.POSIXct(strptime(inflow$SDATE,'%m/%d/%Y'))
 
-# Site 3
-h3 = harp %>% dplyr::filter(site_name == 'HARP INFLOW #3') %>%
-  dplyr::select(Dates = sampledate,Solute = doc)
 
-i3 = inflow %>% dplyr::filter(INFO == 'HARP INFLOW #3') %>%
-  dplyr::select(Dates = SDATE,Flow = DISCHARGE) %>%
-  left_join(h3,'Dates') %>%
-  filter(!is.na(Flow)) %>%
-  filter(Flow > 0) %>%
-  arrange(Dates)
+which.site = 'HARP INFLOW #3'
 
-# As Dates 
-d3 = i3 %>% mutate(Dates = as.Date(Dates))
+inflow.doc = function(lakename,which.site){
+  # Site 3
+  h3 = harp %>% dplyr::filter(site_name == which.site) %>%
+    dplyr::select(Dates = sampledate,Solute = doc)
+  
+  i3 = inflow %>% dplyr::filter(INFO == which.site) %>%
+    dplyr::select(Dates = SDATE,Flow = DISCHARGE) %>%
+    left_join(h3,'Dates') %>%
+    dplyr::filter(!duplicated(Dates)) %>%
+    filter(!is.na(Flow)) %>%
+    filter(Flow > 0) %>%
+    arrange(Dates)
+  
+  # As Dates 
+  d3 = i3 %>% mutate(Dates = as.Date(Dates))
+  
+  # Remove NA
+  s3 = i3 %>% filter(!is.na(Solute)) %>%
+    mutate(Dates = as.Date(Dates))
+  
+  a = selBestModel("Solute", data = s3, flow = "Flow",
+                   dates = "Dates", conc.units="mg/L")
+  
+  print(paste0('best model = ',(a$model.no)))
+  
+  # Models 
+  lr9 <- loadReg2(loadReg(Solute ~ model(9), data = s3, flow = "Flow",
+                 dates = "Dates",conc.units="mg/L"))
+  lrC <- loadComp(reg.model = lr9, interp.format = "conc",
+                  interp.data = s3)
+  
+  # Summaries 
+  # summary(getFittedModel(lr9))
+  # qplot(x=Date, y=Resid, data=getResiduals(lr9, newdata=s3))
+  # residDurbinWatson(lr9, "conc", newdata=s3, irreg=TRUE)
+  # estimateRho(lr9, "conc", newdata=s3, irreg=TRUE)$rhoestimateRho(no3_reg2, "conc", newdata=intdat, irreg=TRUE)$rho
+  getCorrectionFraction(lrC, "flux", newdat=s3)
+  
+  # Make predictions
+  # ----------------------
+  p.lr = predictSolute(lr9, "conc", d3, se.pred=TRUE, date=TRUE)
+  p.lc = predictSolute(lrC, "conc", d3, se.pred=TRUE, date=TRUE)
+  
+  
+  # Plot
+  png(paste0(lakename,'/fig_',which.site,'.png'),
+      width = 7,height = 5,units = 'in',res = 300)
+    par(mar=c(3,3,3,1),mgp=c(1.5,0.5,0))
+    plot(d3$Dates,d3$Solute,type='o',col='red3',pch=16,
+         xlab = 'Date',ylab = 'DOC (mg/L)',main = which.site)
+    lines(p.lr$date,p.lr$fit,type='o',col='navy',pch=16,cex=0.2)
+    lines(p.lc$date,p.lc$fit,type='o',col='seagreen',pch=16,cex=0.2)
+    legend('topleft',fill = c('red3','navy','seagreen'),
+           legend = c('Observed','Model - Reg','Model - Comp'),
+           bty='n')
+  dev.off()
+  
+  
+  write.csv(p.lr,paste0(lakename,'/loadestReg_',which.site,'.csv'),row.names = F)
+  write.csv(p.lc,paste0(lakename,'/loadestComp_',which.site,'.csv'),row.names = F)
+  write.csv(d3,paste0(lakename,'/observed_',which.site,'.csv'),row.names = F)
+}
 
-# Remove NA
-s3 = i3 %>% filter(!is.na(Solute)) %>%
-  mutate(Dates = as.Date(Dates))
-
-a = selBestModel("Solute", data = s3, flow = "Flow",
-                 dates = "Dates", conc.units="mg/L")
-
-print(paste0('best model = ',(a$model.no)))
-
-# Models 
-lr9 <- loadReg2(loadReg(Solute ~ model(9), data = s3, flow = "Flow",
-               dates = "Dates",conc.units="mg/L"))
-lrC <- loadComp(reg.model = lr9, interp.format = "conc",
-                interp.data = s3)
-
-# Summaries 
-summary(getFittedModel(lr9))
-qplot(x=Date, y=Resid, data=getResiduals(lr9, newdata=s3))
-residDurbinWatson(lr9, "conc", newdata=s3, irreg=TRUE)
-estimateRho(lr9, "conc", newdata=s3, irreg=TRUE)$rhoestimateRho(no3_reg2, "conc", newdata=intdat, irreg=TRUE)$rho
-getCorrectionFraction(lrC, "flux", newdat=s3)
-
-# Make predictions
-# ----------------------
-p.lr = predictSolute(lr9, "conc", d3, se.pred=TRUE, date=TRUE)
-p.lc = predictSolute(lrC, "conc", d3, se.pred=TRUE, date=TRUE)
-
-
-# Plot
-png('loadestDOC_HARP.png',width = 7,height = 5,units = 'in',res = 300)
-  par(mar=c(3,3,3,1),mgp=c(1.5,0.5,0))
-  plot(d3$Dates,d3$Solute,type='o',col='red3',pch=16,
-       xlab = 'Date',ylab = 'DOC (mg/L)',main = 'Harp Lake Site #3')
-  lines(p.lr$date,p.lr$fit,type='o',col='navy',pch=16,cex=0.2)
-  lines(p.lc$date,p.lc$fit,type='o',col='seagreen',pch=16,cex=0.2)
-  legend('topleft',fill = c('red3','navy','seagreen'),
-         legend = c('Observed','Model - Reg','Model - Comp'),
-         bty='n')
-dev.off()
-
-write.csv(p.lr,'loadestReg_Harp3.csv',row.names = F)
-write.csv(p.lc,'loadestComp_Harp3.csv',row.names = F)
-write.csv(d3,'observed_Harp3.csv',row.names = F)
+sites = unique(harp$site_name)
+for (i in 1:length(sites)){
+  inflow.doc('Harp',sites[i])
+}
